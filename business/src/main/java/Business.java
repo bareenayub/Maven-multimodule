ea6bf2f4e34c04ac9684a5664c1f937b1b0e2fbc
bareen.ayub@tothenew.com <bareen.ayubtothenew.com>
2021-10-01 15:12:00 +0530

    multi module

---
+++ business/src/main/java/Business.java
@@ -0,0 +1,7 @@
+public class Business {
+    public static void main(String[] args) {
+        System.out.println ("Hello in Bussiness  Module");
+        Data d=new Data ();
+
+    }
+}
---
+++ data/src/main/java/Data.java
@@ -0,0 +1,5 @@
+public class Data {
+    public static void main(String[] args) {
+        System.out.println ("HEllo In Data Module");
+    }
+}
